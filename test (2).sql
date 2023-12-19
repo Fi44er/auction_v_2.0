8fb2457a16be195b0a7a3c50de7f0068c5161beb
Fi44er <vaceslavmalugin81@gmail.com>
2023-12-20 02:30:38 +0500

    add

--- test (2).sql
+++ test (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Дек 19 2023 г., 00:48
+-- Время создания: Дек 20 2023 г., 02:30
 -- Версия сервера: 8.0.35-0ubuntu0.22.04.1
 -- Версия PHP: 8.1.2-1ubuntu2.14
 
@@ -24,25 +24,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `completed_lots`
+-- Структура таблицы `current_lots`
 --
 
-CREATE TABLE `completed_lots` (
-  `id_lot` int NOT NULL,
-  `id_new_owner` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы ` current_lots`
---
-
-CREATE TABLE ` current_lots` (
+CREATE TABLE `current_lots` (
+  `id` int NOT NULL,
   `lot_id` int NOT NULL,
   `id_owner` int NOT NULL,
   `last_buyer_id` int DEFAULT NULL,
-  `current_price` int NOT NULL
+  `current_price` bigint NOT NULL,
+  `bet_history` varchar(6000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '[]'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -55,9 +46,8 @@ CREATE TABLE `lots` (
   `id` int NOT NULL,
   `name` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
   `img` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
-  `initial_bid` int NOT NULL,
-  `description` longtext COLLATE utf8mb4_general_ci NOT NULL,
-  `start_date` varchar(10) COLLATE utf8mb4_general_ci NOT NULL
+  `initial_bid` bigint NOT NULL,
+  `description` longtext COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -77,23 +67,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Дамп данных таблицы `users`
---
-
-INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`, `balance`) VALUES
-(2, 'Иванов Иван Иванович', 'test', 'test@local.ru', '202cb962ac59075b964b07152d234b70', 'uploads/15698233144.png', 0),
-(4, 'wfefewff', 'qwerty', 'efewfewf@mail.ru', '698d51a19d8a121ce581499d7b701668', 'uploads/1702589008Снимок экрана от 2023-12-03 18-30-37.png', 0),
-(5, 'qwerty', 'qwerty1', 'qwerty@mail.ru', 'c4ca4238a0b923820dcc509a6f75849b', 'uploads/1702589394Снимок экрана от 2023-12-03 18-30-37.png', 0),
-(6, 'root', 'root', 'root@mail.ru', '63a9f0ea7bb98050796b649e85481845', 'uploads/1702755835Снимок экрана от 2023-12-03 18-30-37.png', 25491);
-
---
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы ` current_lots`
+-- Индексы таблицы `current_lots`
 --
-ALTER TABLE ` current_lots`
+ALTER TABLE `current_lots`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `id_owner` (`id_owner`),
   ADD KEY `last_buyer_id` (`last_buyer_id`),
   ADD KEY `lot_id` (`lot_id`);
@@ -115,28 +96,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `current_lots`
+--
+ALTER TABLE `current_lots`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT для таблицы `lots`
 --
 ALTER TABLE `lots`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
 
 --
--- Ограничения внешнего ключа таблицы ` current_lots`
+-- Ограничения внешнего ключа таблицы `current_lots`
 --
-ALTER TABLE ` current_lots`
-  ADD CONSTRAINT ` current_lots_ibfk_1` FOREIGN KEY (`id_owner`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT ` current_lots_ibfk_2` FOREIGN KEY (`last_buyer_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT ` current_lots_ibfk_3` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `current_lots`
+  ADD CONSTRAINT `current_lots_ibfk_1` FOREIGN KEY (`id_owner`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `current_lots_ibfk_2` FOREIGN KEY (`last_buyer_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `current_lots_ibfk_3` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
